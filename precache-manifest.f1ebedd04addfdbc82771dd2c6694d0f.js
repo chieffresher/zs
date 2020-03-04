1c4bef960302a160ffcb638f4b99ad408ec03a40
chieffresher@github.com <chieffresher@yahoo.com>
2020-03-04 14:21:43 +0000

    first commit zs

---
+++ precache-manifest.f1ebedd04addfdbc82771dd2c6694d0f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384cb184b726eb1fa231f862866a3bc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "144441948fb83f8fe43e",
+    "url": "/static/css/main.8262c7b4.chunk.css"
+  },
+  {
+    "revision": "34322b194ba797bf7e19",
+    "url": "/static/js/2.c1d7063e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.c1d7063e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "144441948fb83f8fe43e",
+    "url": "/static/js/main.fcbf69e2.chunk.js"
+  },
+  {
+    "revision": "7e5dfe3f608176130e53",
+    "url": "/static/js/runtime-main.c9e462a2.js"
+  },
+  {
+    "revision": "083e6d37860b5c158be4c9bd7cd8f8e9",
+    "url": "/static/media/AD2.083e6d37.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
